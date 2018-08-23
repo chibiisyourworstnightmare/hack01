66a0aaf4694464913c8af0e4c0d3608ca2700c5e
bdose2 <42591078+bdose2@users.noreply.github.com>
2018-08-23 14:56:06 -0500

    added my name

--- hello.c
+++ hello.c
@@ -11,6 +11,6 @@
 int main(int argc, char **argv) {
 
   printf("Hello, my name is Kelvin and I'm majoring in Computer Engineering!\n");
-
+  printf("Hello my name is bradyn and i am doing computer engineering\n");
   return 0;
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 url: https://github.com/chibiisyourworstnightmare/hack01
-authors: kelvin Wertz
+authors: kelvin Wertz, Bradyn Dose
